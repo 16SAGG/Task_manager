a48e08a6d2ab3429d906968a518adb38f28d7bab
Samuel <sagg160800@gmail.com>
2022-12-27 11:26:02 -0400

    changes_buildsh

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -o errexit
 
 # poetry install
 
-#python manage.py collectstatic --no-input
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
 python manage.py migrate
